88844357372b30c9c3fda2a6f7d63f419433c954
TheSirsh <sirsh@bk.ru>
2023-06-15 19:16:27 +0300

    feat: add labels sectionstyles

--- src/components/labels/labels.vue
+++ src/components/labels/labels.vue
@@ -15,3 +15,27 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  section {
+    display: flex;
+    flex-direction: column;
+    margin: calc(100vw / 12) auto;
+    width: calc(100vw / 1.53);
+    height: calc(100vw / 4.74)
+  }
+
+  h2 {
+    font-family: 'Poppins';
+    font-size: calc(100vw / 36);
+    font-weight: bold;
+    margin: 0px;
+  }
+
+  p {
+    font-size: calc(100vw / 72);
+    margin: calc(100vw / 144) auto;
+    font-weight: 500;
+    opacity: 0.5;
+  }
+</style>
--- src/components/labels/marks.vue
+++ src/components/labels/marks.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="main-container">
     <div>
       <img src="@/assets/svg/labels-gas.svg" alt="Gas">
       <p>No Gas Fees</p>
@@ -20,3 +20,23 @@
 
   }
 </script>
+
+<style scoped>
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+    margin: calc(100vw / 22.86) auto;
+    width: calc(100vw / 1.53);
+    height: calc(100vw / 10.83);
+  }
+
+  p {
+    margin-top: calc(100vw / 102.86);
+    font-size: calc(100vw / 72);
+    font-weight: 600;
+  }
+
+  img {
+    height: calc(100vw / 19.2);
+  }
+</style>
