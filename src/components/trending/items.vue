01c3f30ad7b5fb3c631057a1b2d015fe0bc37016
TheSirsh <sirsh@bk.ru>
2023-06-19 22:55:11 +0300

    feat: add trending section styles

--- src/components/trending/items.vue
+++ src/components/trending/items.vue
@@ -1,54 +1,61 @@
 <template>
-  <div>
-    <div>
-      <img src="@/assets/jpg/trending-img1.jpg" alt="">
-      <div>
-        <span>@Johny</span>
-        <span>Current Bid</span>
+  <div class="main-container">
+
+    <div class="container">
+      <img src="@/assets/jpg/trending-img1.jpg" alt="" class="pict">
+      <div class="upper-text">
+        <span class="upper-text_left">@Johny</span>
+        <span class="upper-text_right">Current Bid</span>
       </div>
-      <div>
+      <div class="middle-text">
         <span>Lorem Ipsum</span>
-        <span>0.005 ETH</span>
+        <span>>0.005 ETH</span>
       </div>
-      <div>
-        <img src="@/assets/svg/panel-heart.svg" alt="">
-        <button>
+      <div class="bottom-text">
+        <button class="button-like">
+          <img src="@/assets/svg/panel-unheart.svg" alt="" class="img-like">
+        </button>
+        <button class="button-buy">
           <span>Buy Now</span>
         </button>
       </div>
     </div>
 
-    <div>
-      <img src="@/assets/jpg/trending-img3.jpg" alt="">
-      <div>
-        <span>@Johny</span>
-        <span>Current Bid</span>
+    <div class="container">
+      <img src="@/assets/jpg/trending-img2.jpg" alt="" class="pict">
+      <div class="upper-text">
+        <span class="upper-text_left">@Johny</span>
+        <span class="upper-text_right">Current Bid</span>
       </div>
-      <div>
+      <div class="middle-text">
         <span>Lorem Ipsum</span>
-        <span>0.005 ETH</span>
+        <span>>0.005 ETH</span>
       </div>
-      <div>
-        <img src="@/assets/svg/panel-heart.svg" alt="">
-        <button>
+      <div class="bottom-text">
+        <button class="button-like">
+          <img src="@/assets/svg/panel-unheart.svg" alt="" class="img-like">
+        </button>
+        <button class="button-buy">
           <span>Buy Now</span>
         </button>
       </div>
     </div>
 
-    <div>
-      <img src="@/assets/jpg/trending-img2.jpg" alt="">
-      <div>
-        <span>@Johny</span>
-        <span>Current Bid</span>
+    <div class="container">
+      <img src="@/assets/jpg/trending-img3.jpg" alt="" class="pict">
+      <div class="upper-text">
+        <span class="upper-text_left">@Johny</span>
+        <span class="upper-text_right">Current Bid</span>
       </div>
-      <div>
+      <div class="middle-text">
         <span>Lorem Ipsum</span>
-        <span>0.005 ETH</span>
+        <span>>0.005 ETH</span>
       </div>
-      <div>
-        <img src="@/assets/svg/panel-heart.svg" alt="">
-        <button>
+      <div class="bottom-text">
+        <button class="button-like">
+          <img src="@/assets/svg/panel-unheart.svg" alt="" class="img-like">
+        </button>
+        <button class="button-buy">
           <span>Buy Now</span>
         </button>
       </div>
@@ -62,3 +69,101 @@
 
   }
 </script>
+
+<style scoped>
+  .main-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: calc(100vw / 96);
+    width: 100%;
+    height: calc(100vw / 0.79);
+    margin: 0px auto;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: calc(100vw / 3.7);
+    height: calc(100vw / 2.42);
+    background-color: rgba(255, 255, 255, 0.1);
+    font-family: "Popins";
+    border-radius: calc(100vw / 72);
+  }
+
+  .pict {
+    width: calc(100vw / 4.27);
+    height: calc(100vw / 3.79);
+    border-radius: calc(100vw / 144);
+    margin-top: calc(100vw / 55.38);
+  }
+
+  .upper-text {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: calc(100vw / 4.27);
+    opacity: 0.5;
+    font-weight: medium;
+  }
+
+  .upper-text_left {
+    font-size: calc(100vw / 72);
+  }
+
+  .upper-text_right {
+    font-size: calc(100vw / 96);
+  }
+
+  .middle-text {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: calc(100vw / 4.27);
+    font-family: "Poppins";
+    font-weight: bold;
+    font-size: calc(100vw / 72);
+  }
+
+  .bottom-text {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: calc(100vw / 4.27);
+    margin-bottom: calc(100vw / 57.6);
+  }
+
+  .button-like {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: calc(100vw / 53.33);
+    height: calc(100vw / 60);
+    background: none;
+    border: none;
+    padding: 0;
+  }
+
+  .img-like {
+    width: 100%;
+    height: 100%;
+  }
+
+  .button-buy {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: calc(100vw / 4.95);
+    height: calc(100vw / 28.8);
+    background: linear-gradient(to top right, #671ae4, #b75cff);
+    border: none;
+    border-radius: calc(100vw / 144);
+  }
+
+  span {
+    font-size: calc(100vw / 72);
+    font-weight: semibold;
+    color: #ffffff;
+  }
+</style>
--- src/components/trending/trending.vue
+++ src/components/trending/trending.vue
@@ -15,3 +15,29 @@
     }
   }
 </script>
+
+<style scoped>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: calc(100vw / 1.2);
+    height: calc(100vw / 1.91);
+    margin: calc(100vw / 9.6) auto;
+    font-family: "Poppins";
+  }
+
+  h2 {
+    margin: 0;
+    font-size: calc(100vw / 36);
+    font-weight: bold;
+  }
+
+  p {
+    margin: calc(100vw / 144) auto calc(100vw / 48) auto;
+    width: calc(100vw / 3.4);
+    font-size: calc(100vw / 72);
+    font-weight: medium;
+    opacity: 0.5;
+  }
+</style>
