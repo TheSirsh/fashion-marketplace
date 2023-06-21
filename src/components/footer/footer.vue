02d71ff9ae38b827b5b4d5b6c34382485c029cd0
TheSirsh <sirsh@bk.ru>
2023-06-21 20:39:23 +0300

    feat: add footer section styles

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -2,7 +2,9 @@
   <footer>
     <div>
       <h2>LOGO</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque donec non pellentesque ut.</p>
+      <ul>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque donec non pellentesque ut.</li>
+      </ul>
     </div>
 
     <div>
@@ -27,9 +29,11 @@
 
     <div>
       <h3>Contact</h3>
-      <p>+012 3456789</p>
-      <p>adorableprogrammer@gmail.com</p>
-      <div>
+      <ul>
+        <li>+012 3456789</li>
+        <li>adorableprogrammer@gmail.com</li>
+      </ul>
+      <div class="social-container">
         <img src="@/assets/svg/footer-youtube.svg" alt="">
         <img src="@/assets/svg/footer-discord.svg" alt="">
         <img src="@/assets/svg/footer-instagram.svg" alt="">
@@ -37,3 +41,64 @@
     </div>
   </footer>
 </template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  footer {
+    display: flex;
+    justify-content: space-between;
+    width: calc(100vw / 1.2);
+    height: calc(100vw / 7.13);
+    margin: calc(100vw / 11.08) auto calc(100vw / 28.5) auto;
+  }
+
+  h2 {
+    font-size: calc(100vw / 72);
+    margin: 0;
+    padding: 0;
+    font-weight: bold;
+    color: #6c5dd3;
+    text-align: left;
+  }
+
+  h3 {
+    font-size: calc(100vw / 72);
+    margin: 0;
+    padding: 0;
+    font-weight: bold;
+    text-align: left;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    max-width: calc(100vw / 5);
+  }
+
+  li {
+    font-size: calc(100vw / 96);
+    font-weight: medium;
+    list-style: none;
+    opacity: 0.5;
+    text-align: left;
+    margin: calc(100vw / 72) 0px;
+  }
+
+  .social-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: calc(100vw / 9.29);
+    height: calc(100vw / 57.6);
+  }
+
+  img {
+    width: calc(100vw / 57.6);
+    height: calc(100vw / 57.6);
+  }
+</style>
--- src/components/start/start.vue
+++ src/components/start/start.vue
@@ -12,3 +12,36 @@
     
   }
 </script>
+
+<style scoped>
+  section {
+    width: calc(100vw / 1.20);
+    height: calc(100vw / 4.5);
+    background: linear-gradient(to top right, #671ae4, #b75cff);
+    border-radius: calc(100vw / 144);
+    margin: calc(100vw / 9.6) auto calc(100vw / 11.08) auto;
+  }
+
+  h2 {
+    width: calc(100vw / 3.63);
+    height: calc(100vw / 9.6);
+    margin: 0px auto;
+    padding-top: calc(100vw / 28.8);
+    font-size: calc(100vw / 28.8);
+    font-weight: bold;
+  }
+
+  button {
+    width: calc(100vw / 9.29);
+    height: calc(100vw / 28.8);
+    border: none;
+    border-radius: calc(100vw / 144);
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+
+  span {
+    font-size: calc(100vw / 72);
+    font-weight: medium;
+    color: #ffffff;
+  }
+</style>
