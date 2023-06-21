ec7c222122868584c935b29cfaeda4fa097a017a
TheSirsh <sirsh@bk.ru>
2023-06-21 19:20:48 +0300

    feat: add faq section styles

--- src/components/faq/board.vue
+++ src/components/faq/board.vue
@@ -14,3 +14,26 @@
 
   }
 </script>
+
+<style scoped>
+  div {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: calc(100vw / 96);
+    width: 100%;
+    height: calc(100vw / 5.76);
+  }
+
+  input {
+    width: calc(100vw / 2.44);
+    height: calc(100vw / 28.8);
+    background: none;
+    border: none;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.5)
+  }
+
+  input::placeholder {
+    font-size: calc(100vw / 72);
+    font-weight: medium;
+  }
+</style>
--- src/components/faq/faq.vue
+++ src/components/faq/faq.vue
@@ -3,7 +3,6 @@
     <h2>Freaquently Asked Question</h2>
     <p>Wanna Ask Something?</p>
     <board/>
-
   </section>
 </template>
 
@@ -16,3 +15,26 @@
     }
   }
 </script>
+
+<style scoped>
+  section {
+    width: calc(100vw / 1.20);
+    height: calc(100vw / 2.98);
+    margin: calc(100vw / 12) auto calc(100vw / 96) auto;
+  }
+
+  h2 {
+    width: calc(100vw / 3.72);
+    font-size: calc(100vw / 36);
+    font-weight: bold;
+    margin: 0px auto;
+  }
+
+  p {
+    width: calc(100vw / 5.98);
+    margin: calc(100vw / 144) auto calc(100vw / 28.8) auto;
+    font-weight: calc(100vw / 72);
+    font-weight: medium;
+    opacity: 0.5;
+  }
+</style>
