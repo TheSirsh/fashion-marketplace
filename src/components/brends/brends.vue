65973cbffaf564c65326a1ac7a324214908eee30
TheSirsh <sirsh@bk.ru>
2023-06-22 20:24:31 +0300

    feat: brands section converted to an arry

--- src/components/brends/brends.vue
+++ src/components/brends/brends.vue
@@ -1,14 +1,25 @@
 <template>
   <section>
-    <img src="@/assets/svg/adidas.svg" alt="Adidas">
-    <img src="@/assets/svg/puma.svg" alt="Puma">
-    <img src="@/assets/svg/lacoste.svg" alt="Lacoste">
+    <img v-for="image of images" :src="setImage(image.src)" v-bind:alt="image.alt">
   </section>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        images: [
+          {src: "adidas", alt: "adidas"},
+          {src: "puma", alt: "Puma"},
+          {src: "lacoste", alt: "Lacoste"},
+        ],
+      }
+    },
+    methods: {
+      setImage(src) {
+        return require(`@/assets/svg/${src}.svg`)
+      }
+    }
   }
 </script>
 
