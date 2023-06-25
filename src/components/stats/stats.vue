128a126d8b3e0ba26667c2aefc35b499b7b4b62f
TheSirsh <sirsh@bk.ru>
2023-06-25 17:25:40 +0300

    feat: add stats section

--- src/components/stats/stats.vue
+++ src/components/stats/stats.vue
@@ -1,5 +1,19 @@
 <template>
   <section>
-    
+    <h2>Top Market Statistics</h2>
+    <p>The top NFTs on _________  , ranked by volume, floor price and other statistics.</p>
+    <div>
+      <button>
+        <span>Marketplace Perfomence</span>
+      </button>
+      <div>
+        <button>
+        <span>Last 7 Days</span>
+        </button>
+        <button>
+          <span>All Categories</span>
+        </button>
+      </div>
+    </div>
   </section>
 </template>
