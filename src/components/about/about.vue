55741e66042c7e7b9f23b76d428c19a088330bc5
TheSirsh <sirsh@bk.ru>
2023-06-26 19:06:15 +0300

    feat: add when section

--- src/components/about/about.vue
+++ src/components/about/about.vue
@@ -2,16 +2,18 @@
   <section>
     <who/>
     <what/>
+    <when/>
   </section>
 </template>
 
 <script>
   import who from './who.vue';
   import what from './what.vue';
+  import when from './when.vue';
 
   export default {
     components: {
-      who, what,
+      who, what, when,
     },
   }
 </script>
---
+++ src/components/about/when.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div>
+      <img src="@/assets/jpg/when-img1.jpg" alt="1">
+      <img src="@/assets/jpg/when-img2.jpg" alt="2">
+      <img src="@/assets/jpg/when-img3.jpg" alt="3">
+    </div>
+    <div>
+      <h2>When We Started</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate tortor ipsum neque nulla. Et turpis scelerisque vulputate elementum, egestas neque scelerisque turpis. Adipiscing lacus vel lobortis sit tincidunt enim, libero. In dignissim tortor sit sit convallis morbi.</p>
+      <button>
+        <span>More +</span>
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
