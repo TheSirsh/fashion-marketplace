7f1f0fffd60d9719ce217fc6d6db13d9ceff1b06
TheSirsh <sirsh@bk.ru>
2023-06-26 18:24:50 +0300

    feat: add a table to the stats section styles

--- src/components/stats/board.vue
+++ src/components/stats/board.vue
@@ -1,7 +1,7 @@
 <template>
   <table>
     <tr>
-      <th>Collection</th>
+      <th class="fth">Collection</th>
       <th>Volume</th>
       <th>24H%</th>
       <th>7D%</th>
@@ -9,13 +9,25 @@
       <th>Owners</th>
       <th>Items</th>
     </tr>
-    <tr v-for="stat in stats">
-      <td>{{ stat.collection }}</td>
-      <td>{{ stat.volume }}</td>
-      <td>{{ stat.day }}</td>
-      <td>{{ stat.week }}</td>
-      <td>{{ stat.price }}</td>
-      <td>{{ stat.owner }}</td>
+    <tr v-for="stat in stats" class="data">
+      <td>
+        <div class="container">
+          <span class="number">{{ stat.id }}</span>
+          <img :src="setImage(stat.img)" :alt="stat.alt" class="img">
+          <span class="label">{{ stat.collection }}</span>
+        </div>
+      </td>
+      <td>
+        <img src="@/assets/svg/etherium.svg" alt="Etherium">
+        {{ stat.volume }}
+      </td>
+      <td class="green">{{ stat.day }}</td>
+      <td class="red">{{ stat.week }}</td>
+      <td>
+        <img src="@/assets/svg/etherium.svg" alt="Etherium" class="etherium">
+        {{ stat.price }}
+      </td>
+      <td>{{ stat.owners }}</td>
       <td>{{ stat.items }}</td>
     </tr>
 
@@ -27,15 +39,94 @@
     data() {
       return {
         stats: [
-          { collection: "Adidas", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
-          { collection: "Nike", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
-          { collection: "Lacoste", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
-          { collection: "Zalando", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
-          { collection: "Zara", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
-          { collection: "Cartier", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
-          { collection: "Hermes", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
+          { id: "1", img: "1", alt: "Adidas", collection: "Adidas", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
+          { id: "2", img: "2", alt: "Nike", collection: "Nike", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
+          { id: "3", img: "3", alt: "Lacoste", collection: "Lacoste", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
+          { id: "4", img: "4", alt: "Zalando", collection: "Zalando", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
+          { id: "5", img: "5", alt: "Zara", collection: "Zara", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
+          { id: "6", img: "6", alt: "Cartier", collection: "Cartier", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
+          { id: "7", img: "7", alt: "Hermes", collection: "Hermes", volume: "27,966.76", day: "+92.96", week: "-16.38", price: "12.99", owners: "5.9K", items: "10K", },
         ],
       }
     },
+    methods: {
+      setImage(src) {
+        return require(`@/assets/jpg/stats-img${src}.jpg`)
+      }
+    }
   }
 </script>
+
+<style scoped>
+  table {
+    width: calc(100vw / 1.14);
+    height: calc(100vw / 1.6);
+    font-size: 0.9rem;
+    border-collapse: collapse;
+    margin: calc(100vw / 14.4) auto calc(100vw / 28.8) auto;
+  }
+
+  th {
+    font-weight: 600;
+    border-bottom: 0.5px solid #ffffff;
+    margin: 0;
+    padding: 0;
+  }
+
+  td {
+    border-bottom: 0.5px solid rgba(255, 255, 255, 0.3);
+    font-weight: 500;
+  }
+
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: start;
+  }
+
+  .number {
+    display: inline-block;
+    font-size: 1.2rem;
+    font-weight: 700;
+  }
+
+  .img {
+    display: inline-block;
+    width: calc(100vw / 28.8);
+    height: calc(100vw / 28.8);
+    border-radius: 50%;
+    margin: 0px calc(100vw / 144) 0px calc(100vw / 57.6);
+  }
+
+  .label {
+    display: inline-block;
+    font-size: 0.9rem;
+  }
+
+  .fth {
+    text-align: left;
+    padding-left: calc(100vw / 53.33);
+  }
+
+  tr:first-child{
+    height: calc(100vw / 20.28);
+  }
+
+  .data {
+    height: calc(100vw / 12);
+  }
+
+  .etherium {
+    width: calc(100vw / 130.91);
+    height: calc(100vw / 84.71);
+    margin-right: calc(100vw / 180);
+  }
+
+  .green{
+    color: #0acf41
+  }
+
+  .red {
+    color: #ff2222
+  }
+</style>
--- src/components/stats/stats.vue
+++ src/components/stats/stats.vue
@@ -2,25 +2,25 @@
   <section>
     <h2>Top Market Statistics</h2>
     <p>The top NFTs on _________  , ranked by volume, floor price and other statistics.</p>
-    <div>
-      <button>
+    <div class="buttons">
+      <button class="button">
         <span>Marketplace Perfomence</span>
       </button>
-      <div>
-        <button>
-        <span>Last 7 Days</span>
+      <div class="selectors-upper">
+        <button class="selector">
+          <span>Last 7 Days</span>
         </button>
-        <button>
+        <button class="selector">
           <span>All Categories</span>
         </button>
       </div>
     </div>
     <board/>
-    <div>
-      <button>
+    <div class="selectors-bottom">
+      <button class="selector2">
         <span>&lt; 1-100</span>
       </button>
-      <button>
+      <button class="selector2">
         <span>101-200 &gt;</span>
       </button>
     </div>
@@ -36,3 +36,78 @@
     }
   }
 </script>
+
+<style scoped>
+  section {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 87.85%;
+    height: calc(100vw / 1.06);
+    margin: calc(100vw / 7.06) auto calc(100vw / 11.07) auto;
+  }
+
+  h2 {
+    font-size: 3rem;
+    font-weight: 800;
+    margin: 0px auto;
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight: 500;
+    margin: calc(100vw / 96) auto calc(100vw / 16) auto;
+    opacity: 0.5;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    margin: 0px auto;
+    width: 86.25%;
+  }
+
+  .button {
+    width: calc(100vw / 7.2);
+    height: calc(100vw / 30.64);
+    border-radius: calc(100vw / 144);
+    background: linear-gradient(to top right, #671ae4, #b75cff);
+    border: none;
+  }
+
+  .selectors-upper {
+    display: flex;
+    justify-content: space-between;
+    width: calc(100vw / 3.98);
+  }
+
+  .selector {
+    width: calc(100vw / 8.42);
+    height: calc(100vw / 30.64);
+    background: none;
+    border: solid 0.3px #ffffff;
+    border-radius: calc(100vw / 288);
+  }
+
+  .selectors-bottom {
+    display: flex;
+    justify-content: space-between;
+    width: calc(100vw / 6.26);
+    margin: 0px auto;
+  }
+
+  .selector2 {
+    width: calc(100vw / 14.4);
+    height: calc(100vw / 36.92);
+    background: none;
+    border: 0.5px solid rgba(255, 255, 255, 0.5);
+    border-radius: calc(100vw / 288);
+    margin: 0px auto calc(100vw / 11.08) auto;
+  }
+
+  span {
+    font-size: 0.8rem;
+    font-weight: 400;
+    color: #ffffff;
+  }
+</style>
