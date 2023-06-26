8e0e39ee43420211e9fe50b3621a86985b79dbf1
TheSirsh <sirsh@bk.ru>
2023-06-26 19:01:33 +0300

    feat: add what section

--- src/components/about/about.vue
+++ src/components/about/about.vue
@@ -1,15 +1,17 @@
 <template>
   <section>
     <who/>
+    <what/>
   </section>
 </template>
 
 <script>
   import who from './who.vue';
+  import what from './what.vue';
 
   export default {
     components: {
-      who,
+      who, what,
     },
   }
 </script>
---
+++ src/components/about/what.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <div>
+      <p>Since 2014</p>
+      <h2>What We Do</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate tortor ipsum neque nulla. Et turpis scelerisque vulputate elementum, egestas neque scelerisque turpis. Adipiscing lacus vel lobortis sit tincidunt enim, libero. In dignissim tortor sit sit convallis morbi.</p>
+      <button>
+        <span>More +</span>
+      </button>
+    </div>
+    <div>
+      <img src="@/assets/jpg/what-img1.jpg" alt="1">
+      <img src="@/assets/jpg/what-img2.jpg" alt="2">
+      <img src="@/assets/jpg/what-img3.jpg" alt="3">
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
