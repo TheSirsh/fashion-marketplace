948c915a2f80fe30f542e50974ec4db1d3661252
TheSirsh <sirsh@bk.ru>
2023-06-27 14:26:19 +0300

    feat: add when section styles

--- src/components/about/when.vue
+++ src/components/about/when.vue
@@ -1,11 +1,11 @@
 <template>
   <section>
-    <div>
-      <img src="@/assets/jpg/when-img1.jpg" alt="1">
-      <img src="@/assets/jpg/when-img2.jpg" alt="2">
-      <img src="@/assets/jpg/when-img3.jpg" alt="3">
+    <div class="img-container">
+      <img src="@/assets/jpg/when-img1.jpg" alt="1" class="img1">
+      <img src="@/assets/jpg/when-img2.jpg" alt="2" class="img2">
+      <img src="@/assets/jpg/when-img3.jpg" alt="3" class="img3">
     </div>
-    <div>
+    <div class="text-container">
       <h2>When We Started</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate tortor ipsum neque nulla. Et turpis scelerisque vulputate elementum, egestas neque scelerisque turpis. Adipiscing lacus vel lobortis sit tincidunt enim, libero. In dignissim tortor sit sit convallis morbi.</p>
       <button>
@@ -20,3 +20,80 @@
 
   }
 </script>
+
+<style scoped>
+  section {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+  .text-container {
+    width: calc(100vw / 2.13);
+  }
+
+  h2 {
+    font-size: 3rem;
+    font-weight: 800;
+    margin: calc(100vw / 144) 0px calc(100vw / 72) 0px;
+    text-align: left;
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight: 500;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    opacity: 0.5;
+  }
+
+  button {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: calc(100vw / 13.09);
+    height: calc(100vw / 28.8);
+    margin: calc(100vw / 32) 0px 0px 0px;
+    background: linear-gradient(to top right, #671ae4, #b75cff);
+    border: none;
+    border-radius: calc(100vw / 144);
+  }
+
+  span {
+    font-size: 1rem;
+    color: #ffffff;
+    font-weight: 500;
+  }
+
+  .img-container {
+    position: relative;
+    width: calc(100vw / 3.85);
+    height: calc(100vw / 3.22);
+  }
+
+  img {
+    position: absolute;
+    width: calc(100vw / 4.44);
+    height: calc(100vw / 3.63);
+    border-radius: calc(100vw / 144);
+  }
+
+  .img1 {
+    top: 0;
+    left: 0;
+    z-index: 3;
+  }
+
+  .img2 {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+  }
+
+  .img3 {
+    bottom: 0;
+    right: 0;
+    z-index: 1;
+  }
+</style>
