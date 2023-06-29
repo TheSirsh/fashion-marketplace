aeba9925a220943eb145ad7de870b1accfba1690
TheSirsh <sirsh@bk.ru>
2023-06-29 18:40:20 +0300

    feat: add aside group section

--- src/components/community/aside/aside.vue
+++ src/components/community/aside/aside.vue
@@ -2,15 +2,17 @@
   <aside>
     <input type="text" placeholder="Search Posts">
     <follow/>
+    <group/>
   </aside>
 </template>
 
 <script>
   import follow from './follow.vue';
+  import group from './group.vue';
 
   export default {
     components: {
-      follow
+      follow, group,
     }
   }
 </script>
---
+++ src/components/community/aside/group.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3>Suggested Groups</h3>
+    <div v-for="group in groups">
+      <div>
+        <img :src="setImage(group.imgSrc)" :alt="group.imgAlt">
+        <p>{{ group.name }}</p>
+      </div>
+      <div>
+        <p>{{ group.numder }}</p>
+        <button>
+          <span>{{ group.button }}</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        groups: [
+          { imgSrc: "1", imgAlt: "Zalando", name: "Zalando", numder: "24k M", button: "Join", }, 
+          { imgSrc: "2", imgAlt: "Zara", name: "Zara", numder: "24k M", button: "Join", }, 
+          { imgSrc: "3", imgAlt: "Cartier", name: "Cartier", numder: "24k M", button: "Join", }, 
+        ],
+      }
+    },
+    methods: {
+      setImage(src) {
+        return require(`@/assets/jpg/group${src}.jpg`)
+      },
+    },
+  }
+</script>
