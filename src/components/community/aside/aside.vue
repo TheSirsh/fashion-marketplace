ab11e2eb097098868f55e899ac10dba2c5526088
TheSirsh <sirsh@bk.ru>
2023-06-29 18:52:09 +0300

    feat: add aside trending section

--- src/components/community/aside/aside.vue
+++ src/components/community/aside/aside.vue
@@ -3,16 +3,18 @@
     <input type="text" placeholder="Search Posts">
     <follow/>
     <group/>
+    <trending/>
   </aside>
 </template>
 
 <script>
   import follow from './follow.vue';
   import group from './group.vue';
+  import trending from './trending.vue';
 
   export default {
     components: {
-      follow, group,
+      follow, group, trending,
     }
   }
 </script>
---
+++ src/components/community/aside/trending.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h3>Trendings</h3>
+    <div v-for="trending in trendings">
+      <div>
+        <img :src="setImage(trending.imgSrc)" :alt="trending.imgAlt">
+        <div>
+          <p>{{ trending.name }}</p>
+          <p>{{ trending.email }}</p>
+        </div>
+      </div>
+      <p>{{ trending.position }}</p>
+    </div>
+    <p>See All</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        trendings: [
+          { imgSrc: "1", imgAlt: "1", name: "Lorem Ipsum", email: "@farhan khan", position: "#1 Trending", },
+          { imgSrc: "2", imgAlt: "2", name: "Lorem Ipsum", email: "@farhan khan", position: "#2 Trending", },
+          { imgSrc: "3", imgAlt: "3", name: "Lorem Ipsum", email: "@farhan khan", position: "#3 Trending", },
+        ],
+      }
+    },
+    methods: {
+      setImage(src) {
+        return require(`@/assets/jpg/trending${src}.jpg`)
+      },
+    },
+  }
+</script>
