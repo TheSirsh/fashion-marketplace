884333f987eba6cb9442fea74c82686468c6bb8e
TheSirsh <sirsh@bk.ru>
2023-06-29 19:02:35 +0300

    feat: add aside chats section

--- src/components/community/aside/aside.vue
+++ src/components/community/aside/aside.vue
@@ -4,6 +4,7 @@
     <follow/>
     <group/>
     <trending/>
+    <chats/>
   </aside>
 </template>
 
@@ -11,10 +12,11 @@
   import follow from './follow.vue';
   import group from './group.vue';
   import trending from './trending.vue';
+  import chats from './chats.vue';
 
   export default {
     components: {
-      follow, group, trending,
+      follow, group, trending, chats,
     }
   }
 </script>
---
+++ src/components/community/aside/chats.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h3>Recent Chats</h3>
+    <div v-for="chat in chats">
+      <img :src="setImage(chat.imgSrc)" :alt="chat.imgAlt">
+      <div>
+        <div>
+          <span>{{ chat.name }}</span>
+          <span>{{ chat.time }}</span>
+        </div>
+        <p>{{ chat.text }}</p>
+      </div>
+    </div>
+    <p>See All Inbox</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        chats: [
+          { imgSrc: "1", imgAlt: "1", name: "Farhan Khan", time: "11:01 AM", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac, platea aliquet suscipit pellentesque.", },
+          { imgSrc: "2", imgAlt: "2", name: "Farhan Khan", time: "11:01 AM", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac, platea aliquet suscipit pellentesque.", },
+          { imgSrc: "3", imgAlt: "3", name: "Farhan Khan", time: "11:01 AM", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac, platea aliquet suscipit pellentesque.", },
+        ],
+      }
+    },
+    methods: {
+      setImage(src) { 
+        return require(`@/assets/jpg/chats${src}.jpg`)
+      },
+    },
+  }
+</script>
