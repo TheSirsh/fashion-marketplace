c1b4caa29a6c6ee15995cc338474d4be3e32221c
TheSirsh <sirsh@bk.ru>
2023-06-30 19:15:51 +0300

    feat: add aside section styles

--- src/components/community/aside/aside.vue
+++ src/components/community/aside/aside.vue
@@ -1,6 +1,9 @@
 <template>
   <aside>
-    <input type="text" placeholder="Search Posts">
+    <form>
+      <img src="@/assets/svg/search.svg" alt="">
+      <input type="text" placeholder="Search Posts">
+    </form>
     <follow/>
     <group/>
     <trending/>
@@ -20,3 +23,40 @@
     }
   }
 </script>
+
+<style scoped>
+  aside {
+    width: calc(100vw / 4.19);
+    display: flex;
+    flex-direction: column;
+  }
+  
+  form {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: calc(100vw / 30);
+    border: 0.5px solid rgba(255, 255, 255, 0.5);
+    background: none;
+    border-radius: calc(100vw / 7.83);
+  }
+
+  img {
+    width: calc(100vw / 68.57);
+    height: calc(100vw / 68.57);
+    margin-left: calc(100vw / 62.61);
+  }
+
+  input {
+    width: 75%;
+    margin-left: calc(100vw / 96);
+    background: none;
+    border: none;
+  }
+
+  input::placeholder {
+    color: #ffffff;
+    font-size: 0.7rem;
+    font-weight: 400;
+  }
+</style>
