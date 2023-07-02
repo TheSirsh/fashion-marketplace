7deaecaa9c39c2cad3e08f7e0c54c68db8978734
TheSirsh <sirsh@bk.ru>
2023-07-02 16:40:15 +0300

    feat: add creator studio page

--- src/components/creator-studio/creator-studio.vue
+++ src/components/creator-studio/creator-studio.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    !
+    <descr/>
+    <div>
+      <h4>Expand Your Market Like Others,</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate tortor ipsum neque nulla. Et turpis scelerisque vulputate elementum, egestas neque scelerisque turpis.</p>
+    </div>
+    <board/>
+    <div>
+      <button>
+        <span>Create Now</span>
+      </button>
+      <button>
+        <span>Customize Now</span>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+  import descr from "@/components/creator-studio/descr.vue";
+  import board from "@/components/creator-studio/board.vue";
+
   export default {
-    
+    components: {
+      descr, board,
+    }
   }
 </script>
