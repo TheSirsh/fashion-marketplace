d81ab38449d81857e2fb42f6c1237ca61dc832cf
TheSirsh <sirsh@bk.ru>
2023-07-03 13:48:14 +0300

    feat: add explore section

--- src/components/creator-studio/descr/descr.vue
+++ src/components/creator-studio/descr/descr.vue
@@ -2,16 +2,18 @@
   <section>
     <become/>
     <customize/>
+    <explore/>
   </section>
 </template>
 
 <script>
   import become from './become.vue';
   import customize from './customize.vue';
+  import explore from './explore.vue';
 
   export default {
     components: {
-      become, customize,
+      become, customize, explore,
     }
   }
 </script>
---
+++ src/components/creator-studio/descr/explore.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div>
+      <h2>Wear Digital Fashion</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate tortor ipsum neque nulla. Et turpis scelerisque vulputate elementum, egestas neque scelerisque turpis. Adipiscing lacus vel lobortis sit tincidunt enim, libero. In dignissim tortor sit sit convallis morbi.</p>
+      <button>
+        <span>Explore</span>
+      </button>
+    </div>
+    <div>
+      <img src="@/assets/jpg/explore-img1.jpg" alt="1">
+      <img src="@/assets/jpg/explore-img2.jpg" alt="2">
+      <img src="@/assets/jpg/explore-img3.jpg" alt="3">
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
