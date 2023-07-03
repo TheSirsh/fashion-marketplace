65960db742c1f75547ff28626f97f6dff81fc0b2
TheSirsh <sirsh@bk.ru>
2023-07-03 13:44:01 +0300

    feat: add customize section

---
+++ src/components/creator-studio/descr/customize.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div>
+      <img src="@/assets/jpg/customize-img1.jpg" alt="1">
+      <img src="@/assets/jpg/customize-img2.jpg" alt="2">
+      <img src="@/assets/jpg/customize-img3.jpg" alt="3">
+    </div>
+    <div>
+      <h2>Customize Your Existing Store</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate tortor ipsum neque nulla. Et turpis scelerisque vulputate elementum, egestas neque scelerisque turpis. Adipiscing lacus vel lobortis sit tincidunt enim, libero. In dignissim tortor sit sit convallis morbi.</p>
+      <button>
+        <span>Customize Now</span>
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
--- src/components/creator-studio/descr/descr.vue
+++ src/components/creator-studio/descr/descr.vue
@@ -1,15 +1,17 @@
 <template>
   <section>
     <become/>
+    <customize/>
   </section>
 </template>
 
 <script>
   import become from './become.vue';
+  import customize from './customize.vue';
 
   export default {
     components: {
-      become,
+      become, customize,
     }
   }
 </script>
