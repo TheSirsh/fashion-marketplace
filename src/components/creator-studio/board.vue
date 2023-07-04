e73a7663dad946a67786326cdafbcf177e403f71
TheSirsh <sirsh@bk.ru>
2023-07-04 18:28:20 +0300

    feat: add board section styles

--- src/components/creator-studio/board.vue
+++ src/components/creator-studio/board.vue
@@ -1,8 +1,8 @@
 <template>
-  <section>
+  <section class="main-container">
     <div v-for="creator in creators">
-      <img :src="setImage(creator.creatorSrc)" :alt="creator.creatorAlt">
-      <img :src="setImage(creator.photoSrc)" :alt="creator.fotoAlt">
+      <img :src="setImage(creator.creatorSrc)" :alt="creator.creatorAlt" class="bg">
+      <img :src="setImage(creator.photoSrc)" :alt="creator.fotoAlt" class="logo">
       <h5>{{ creator.name }}</h5>
       <p>{{ creator.descr }}</p>
       <button>
@@ -36,3 +36,65 @@
     },
   }
 </script>
+
+<style scoped>
+  .main-container {
+    margin: 0px auto;
+    width: 83.125%;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: calc(100vw / 28.8) calc(100vw / 96);
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: calc(100vw / 144);
+  }
+
+  .bg {
+    width: 100%;
+    height: auto;
+    margin: 0px;
+    border-radius: calc(100vw / 144);
+  }
+
+  .logo {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    width: calc(100vw / 14.4);
+    height: auto;
+  }
+
+  h5 {
+    margin: calc(100vw / 22.5) auto calc(100vw / 144) auto;
+    font-family: "Poppins";
+    font-size: 1rem;
+    font-weight: 700;
+  }
+
+  p {
+    width: 79%;
+    margin: 0px auto;
+    font-size: 0.75rem;
+    font-weight: 500;
+    opacity: 0.5;
+  }
+
+  button {
+    width: 86.63%;
+    height: calc(100vw / 28.8);
+    margin: calc(100vw / 65.45) auto calc(100vw / 34.29) auto;
+    background: linear-gradient(to top right, #671ae4, #b75cff);
+    border: none;
+    border-radius: calc(100vw / 144);
+    font-size: 1rem;
+    font-weight: 500;
+    color: #ffffff;
+  }
+</style>
