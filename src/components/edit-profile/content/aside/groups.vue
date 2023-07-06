6cb00e0b5b704cc5a685f939454bb41e20315978
TheSirsh <sirsh@bk.ru>
2023-07-06 18:30:00 +0300

    feat: add content aside groups section

--- src/components/edit-profile/content/aside/groups.vue
+++ src/components/edit-profile/content/aside/groups.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
-    
+    <div>
+      <p>My Groups</p>
+      <button>
+        <span>Exclusive Groups</span>
+      </button>
+      <div v-for="group in groups">
+        <div>
+          <img :src="setImage(group.imgSrc)" :alt="group.imgAlt">
+          <span>{{ group.brand }}</span>
+        </div>
+        <div>
+          <span>{{ group.subscr }}</span>
+          <button>
+            <span>{{ group.button }}</span>
+          </button>
+        </div>
+      </div>
+      <p>See All Groups</p>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        groups: [
+          { imgSrc: "1", imgAlt: "Zalando", brand: "Zalando", subscr: "24k M", button: "View", },
+          { imgSrc: "2", imgAlt: "Zara", brand: "Zara", subscr: "24k M", button: "View", },
+          { imgSrc: "3", imgAlt: "Cartier", brand: "Cartier", subscr: "24k M", button: "View", },
+        ],
+      }
+    },
+    methods: {
+      setImage(src) {
+        return require(`@/assets/jpg/aside-group${src}.jpg`)
+      }
+    }
   }
 </script>
