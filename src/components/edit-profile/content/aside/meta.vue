bcaf8ccbf9653254859ff6d0b20326c6e69e88bf
TheSirsh <sirsh@bk.ru>
2023-07-06 18:21:35 +0300

    feat: add content aside meta section

--- src/components/edit-profile/content/aside/meta.vue
+++ src/components/edit-profile/content/aside/meta.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    
+    <p>Meta Look</p>
+    <img src="@/assets/jpg/content_aside_meta.jpg" alt="Model">
+    <form>
+      <button>
+        <img src="@/assets/svg/decrease.svg" alt="Decrease">
+      </button>
+      <input type="range">
+      <button>
+        <img src="@/assets/svg/increase.svg" alt="Increase">
+      </button>
+    </form>
   </div>
 </template>
 
