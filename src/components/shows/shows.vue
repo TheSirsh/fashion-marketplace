84a5740314e708e871ed7d4d5cf0ac0db4e64785
TheSirsh <sirsh@bk.ru>
2023-07-11 17:31:17 +0300

    feat: add shows page

--- src/components/shows/shows.vue
+++ src/components/shows/shows.vue
@@ -1,11 +1,28 @@
 <template>
   <section>
-    
+    <div>
+      <button>
+        <span>Established Brands</span>
+      </button>
+      <button>
+        <span>Creators Brands</span>
+      </button>
+    </div>
+    <h2>Fashion Shows</h2>
+    <live/>
+    <upcomings/>
+    <previous/>
   </section>
 </template>
 
 <script>
-  export default {
+  import live from './live.vue';
+  import upcomings from './upcomings.vue';
+  import previous from './previous.vue';
 
+  export default {
+    components: {
+      live, upcomings, previous,
+    }
   }
 </script>
